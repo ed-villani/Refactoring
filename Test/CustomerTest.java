c8a02145b367f65c43157a32264c586ace0d5f41
Eduardo Villani <villani.ed@gmail.com>
2019-05-25 21:26:24 -0300

    Statement em HTML

--- Test/CustomerTest.java
+++ Test/CustomerTest.java
@@ -1,12 +1,13 @@
 import org.junit.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CustomerTest {
 
     @Test
     public void statement() {
-        Movie m1 = new Movie("Pokémon: O Filme", 15);
-        Movie m2 = new Movie("Detetive Pikachu", 12);
+        Movie m1 = new Movie("Pokémon: O Filme", 2);
+        Movie m2 = new Movie("Detetive Pikachu", 1);
 
         Customer c1 = new Customer("Eduardo Villani");
 
@@ -17,9 +18,28 @@ public class CustomerTest {
         c1.addRental(r2);
 
         assertEquals("Rental Record for Eduardo Villani\n" +
-                "	Pokémon: O Filme	0.0\n" +
-                "	Detetive Pikachu	0.0\n" +
-                "Amount owed is 0.0\n" +
+                "	Pokémon: O Filme	1.5\n" +
+                "	Detetive Pikachu	3.0\n" +
+                "Amount owed is 4.5\n" +
                 "You earned 2 frequent renter points", c1.statement());
+    }
+    @Test
+    public void htmlStatement() {
+        Movie m1 = new Movie("Pokémon: O Filme", 2);
+        Movie m2 = new Movie("Detetive Pikachu", 1);
+
+        Customer c1 = new Customer("Eduardo Villani");
+
+        Rental r1 = new Rental(m1, 3);
+        Rental r2 = new Rental(m2, 1);
+
+        c1.addRental(r1);
+        c1.addRental(r2);
+
+        assertEquals("<H1>Rentals for <EM>Eduardo Villani</EM></H1><P>\n" +
+                "Pokémon: O Filme: 1.5<BR>\n" +
+                "Detetive Pikachu: 3.0<BR>\n" +
+                "<P>You owe <EM>4.5</EM><P>\n" +
+                "On this rental you earned <EM>2</EM> frequent renter points<P>", c1.htmlStatement());
     }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -124,4 +124,21 @@ class Customer {
         }
         return result;
     }
+    public String htmlStatement() {
+        Enumeration rentals = _rentals.elements();
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            // show figures for each rental
+            result += each.getMovie().getTitle()+ ": " +
+                    String.valueOf(each.getCharge()) + "<BR>\n";
+        }
+
+        // add footer lines
+        result +=  "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+        result += "On this rental you earned <EM>" +
+                String.valueOf(getTotalFrequentRenterPoints()) +
+                "</EM> frequent renter points<P>";
+        return result;
+    }
 }
